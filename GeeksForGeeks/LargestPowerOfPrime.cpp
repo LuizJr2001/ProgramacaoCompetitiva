6cad9c0fe7c0d06834f0c0081281c05225a62eef
Luiz Carlos dos Santos Júnior <luizcarlossantosjunior2001@gmail.com>
2024-06-01 12:50:01 -0300

    Update

---
+++ GeeksForGeeks/LargestPowerOfPrime.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+const ll MAX = (ll)1e9;
+const ll MIN = -MAX;
+define all(x) x.begin(), x.end()
+define SZ(x) (ll) x.size()
+
+stringstream ss;
+
+int main()
+{
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+
+	return 0;
+}
+
--- ListaMaratonaUFMG/Basicos/CME.cpp
+++ ListaMaratonaUFMG/Basicos/CME.cpp
@@ -23,7 +23,6 @@ stringstream ss;
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-	
 	ll t; cin >> t;
 
 	while(t--)
