68d751b2b540af2a56435cd145fd9de7396ac698
Luiz Carlos dos Santos Júnior <luizcarlossantosjunior2001@gmail.com>
2024-06-01 23:04:34 -0300

    Update

--- ListaMaratonaUFMG/STL/BotasTrocadas.cpp
+++ ListaMaratonaUFMG/STL/BotasTrocadas.cpp
@@ -10,49 +10,46 @@ typedef pair<int, int> pii;
 typedef vector<pii> vii;
 typedef vector<bool> vb;
 typedef vector<string> vs;
-const ll MAX = 10e9 + 100;
-const ll MIN = -MAX;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+#define _ ios::sync_with_stdio(0); cin.tie(0);
 #define all(x) x.begin(), x.end()
 #define SZ(x) (ll) x.size()
+#define f first
+#define s second
 #define dgb(x) cout << #x << '=' << x << '\n';
+#define endl '\n'
 
 stringstream ss;
 
-bool compara(pair<int, string> a, pair<int, string> b)
-{
-	return a.first>b.first;
-}
-
 int main()
-{
-	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-	
-	int n;
+{	_
+
+	int n; 
 	cin >> n;
 
-	vector<pair<int, string>> shoe;
+	vector<pair<int, int>> vec(61, {0, 0});
 
-	for(int i = 0; i < n; i++)
+	for(int i(0); i < n; ++i)
 	{
-		ll m;
-		string l;
+		int m;
+		char l;
 		cin >> m >> l;
 
-		shoe.push_back(make_pair(m, l));
+		if(l == 'D')
+			vec[m].f++;
+		else
+			vec[m].s++;
 
 	}
-	
-	sort(all(shoe), compara);	
+
 	int count(0);
-
-	for(int i = 1; i < n; ++i)
+	for(auto i:vec)
 	{
-		if(shoe[i].first==shoe[i-1].first)
-			if(shoe[i].second + shoe[i-1].second == "DE" || shoe[i].second + shoe[i-1].second == "ED")
-				count++;
-	}	
+		count+=min(i.f, i.s);
+	}
 
-	cout << count << '\n';
+	cout << count << endl;
 
 	return 0;
 }
